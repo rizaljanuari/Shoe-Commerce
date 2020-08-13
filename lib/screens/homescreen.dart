fec433c58afb658ace71ac5146444367c9f47376
rizaljanuari <rizaljanuari4@gmail.com>
2020-08-13 23:48:50 +0700

    adding searchbar plugin

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -8,22 +9,25 @@ class HomeScreen extends StatelessWidget {
         title: Text('ShoeCommerce'),
         centerTitle: true,
         actions: [
-          Icon(
-            Icons.favorite_border
-          ),
+          Icon(Icons.favorite_border),
           SizedBox(
             width: 10,
           ),
-          Icon(
-            Icons.shopping_cart
-          )
+          Icon(Icons.shopping_cart)
         ],
-
       ),
+
+      //untuk Drawer (widget/menu slide ke kiri)
       drawer: Drawer(
         child: Text('drawer'),
       ),
-      
+
+      //untuk body
+      body: FloatingSearchBar(
+          children: [],
+          // title: Text('Search anything...'),
+          trailing: Icon(Icons.search),
+          decoration: InputDecoration.collapsed(hintText: 'Search')),
     );
   }
 }
